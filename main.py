5e9255d485af40b4e904b20db182006106ddf645
Miyai75 <20241256@kyoto-wu.ac.jp>
2022-03-23 11:14:46 +0900

    hello3

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=a))
+        TextSendMessage(text="hello"))
 
 
 if __name__ == "__main__":
