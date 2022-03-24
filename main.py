5e866806b392d0fa3a7ae98811c1a129dab42fab
Miyai75 <20241256@kyoto-wu.ac.jp>
2022-03-24 11:29:49 +0900

    sapo-to2

--- main.py
+++ main.py
@@ -44,86 +44,88 @@ def callback():
 
 
 @handler.add(MessageEvent, message=TextMessage)
-# def __init__(self, name): # インスタンス化されて渡ってくる値を受ける変数を定義します
-#     # インスタンス変数（属性の初期化）
-#     self.name = name # 実行順③
-#     print("コンストラクタが呼ばれました") # # 実行順④]
-def handle_message(event):
-    bus_scene = 0
-    print("Hello World")
-    print(event.message.text)
-    if event.message.text == "京都女子大学の天気":
-        # line_bot_api.reply_message(
-        # event.reply_token,
-        # TextSendMessage(text="天気だね"))
-        weather = tnk.Weather(6110)
-        print(weather)
+
+class SendMessage:
+    def __init__(self): # インスタンス化されて渡ってくる値を受ける変数を定義します
+        # インスタンス変数（属性の初期化）
+        self.self.bus_scene = 0 # 実行順③
+        print("コンストラクタが呼ばれました") # # 実行順④]
+
+    def handle_message(event,self):
+        print("Hello World")
+        print(event.message.text)
+        if event.message.text == "京都女子大学の天気":
+            # line_bot_api.reply_message(
+            # event.reply_token,
+            # TextSendMessage(text="天気だね"))
+            weather = tnk.Weather(6110)
+            print(weather)
+            line_bot_api.reply_message(
+            event.reply_token,
+            [TextSendMessage(text="天気だね"),TextSendMessage(text=weather)])
+
+        # if event.message.text == "大学生活に関する窓口":
+        #     line_bot_api.reply_message(
+        #     event.reply_token,
+        #     [TextSendMessage(text="進路 履修 インターンシップ 奨学金 各種証明書に関する対応窓口に関する情報を教えます！"),TextSendMessage(text="何について知りたいですか？")])
+        #     word = input()
+        #     df = pd.read_csv("center2.csv")#csvファイルを読み込み
+        #     #print(list(df.loc[1]))
+
+        #     a = list#初期値
+
+        #     for index, data in df.iterrows():  # データフレームで行ごとにデータを取得
+        #         #print(index)
+        #         #print(data)
+        #         #print('--------')
+        #         if event.message.text == "進路":
+        #             a = list(df.loc[0])  # csvの1行目は項目なので2行目から数えるので2行目が[0]
+        #             break  # breakしないと6個同じものが出力される
+        #         elif event.message.text == "履修":
+        #             a = list(df.loc[1])
+        #             break
+        #         elif event.message.text == "インターンシップ":
+        #             a = list(df.loc[2])
+        #             break
+        #         elif event.message.text == "学費":
+        #             a = list(df.loc[3])
+        #             break
+        #         elif event.message.text == "奨学金":
+        #             a = list(df.loc[4])
+        #             break
+        #         elif event.message.text == "各種証明書":
+        #             a = list(df.loc[5])
+        #             break
+        #         else:
+        #             a = "いつでもどうぞ"
+        #             break
+            
+        #     line_bot_api.reply_message(
+        #     event.reply_token,
+        #     [TextSendMessage(text=a)])
+
+        if event.message.text == "バスの時刻":
+            print("フラグ")
+            print(self.bus_scene) 
+            line_bot_api.reply_message(
+            event.reply_token,
+            [TextSendMessage(text="登校しますか？下校しますか？"),TextSendMessage(text="1.市バス 2.プリンセスバス")])
+            self.bus_scene = 1
+
+        if self.bus_scene == 1:
+            bus_choices = {1:"市バス", 2:"プリンセスバス"} 
+            if event.message.text in bus_choices:
+                answer1 = bus_choices[event.message.text]
+                print(answer1)
+                line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text=answer1))
+            
         line_bot_api.reply_message(
         event.reply_token,
-        [TextSendMessage(text="天気だね"),TextSendMessage(text=weather)])
+        TextSendMessage(text="hello"))
 
-    if event.message.text == "大学生活に関する窓口":
-        line_bot_api.reply_message(
-        event.reply_token,
-        [TextSendMessage(text="進路 履修 インターンシップ 奨学金 各種証明書に関する対応窓口に関する情報を教えます！"),TextSendMessage(text="何について知りたいですか？")])
-        word = input()
-        df = pd.read_csv("center2.csv")#csvファイルを読み込み
-        #print(list(df.loc[1]))
-
-        a = list#初期値
-
-        for index, data in df.iterrows():  # データフレームで行ごとにデータを取得
-            #print(index)
-            #print(data)
-            #print('--------')
-            if event.message.text == "進路":
-                a = list(df.loc[0])  # csvの1行目は項目なので2行目から数えるので2行目が[0]
-                break  # breakしないと6個同じものが出力される
-            elif event.message.text == "履修":
-                a = list(df.loc[1])
-                break
-            elif event.message.text == "インターンシップ":
-                a = list(df.loc[2])
-                break
-            elif event.message.text == "学費":
-                a = list(df.loc[3])
-                break
-            elif event.message.text == "奨学金":
-                a = list(df.loc[4])
-                break
-            elif event.message.text == "各種証明書":
-                a = list(df.loc[5])
-                break
-            else:
-                a = "いつでもどうぞ"
-                break
-        
-        line_bot_api.reply_message(
-        event.reply_token,
-        [TextSendMessage(text=a)])
-
-    # if event.message.text == "バスの時刻":
-    #     print("フラグ")
-    #     print(bus_scene) 
-    #     line_bot_api.reply_message(
-    #     event.reply_token,
-    #     [TextSendMessage(text="登校しますか？下校しますか？"),TextSendMessage(text="1.市バス 2.プリンセスバス")])
-    #     bus_scene = 1
-
-    # if bus_scene == 1:
-    #     bus_choices = {1:"市バス", 2:"プリンセスバス"} 
-    #     if event.message.text in bus_choices:
-    #         answer1 = bus_choices[event.message.text]
-    #         print(answer1)
-    #         line_bot_api.reply_message(
-    #         event.reply_token,
-    #         TextSendMessage(text=answer1))
-        
-    line_bot_api.reply_message(
-    event.reply_token,
-    TextSendMessage(text="hello"))
-
-
+message = SendMessage()
 if __name__ == "__main__":
 #    app.run()
     port = int(os.getenv("PORT", 5000))
