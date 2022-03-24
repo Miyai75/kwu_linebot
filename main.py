852d682a7ce63619a2377a6d052e26c953f9e446
Miyai75 <20241256@kyoto-wu.ac.jp>
2022-03-24 10:19:07 +0900

    botton

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ from linebot import (
     LineBotApi, WebhookHandler
 )
 from linebot.exceptions import (
-    InvalidSignatureError
+    LineBotApiError, InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage, FlexSendMessage
+    MessageEvent, TextMessage, TextSendMessage
 )
 import os
 import json
@@ -62,168 +62,27 @@ def handle_message(event):
         # jsonopen = open('bus.json', 'r')
         # tmp = json.load(jsonopen)
         
-        payload = """{
-        "type": "flex",
-        "altText": "Flex Message",
-        "contents": {
-            "type": "bubble",
-            "hero": {
-            "type": "image",
-            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
-            "size": "full",
-            "aspectRatio": "20:13",
-            "aspectMode": "cover",
-            "action": {
-                "type": "uri",
-                "label": "Line",
-                "uri": "https://linecorp.com/"
-            }
-            },
-            "body": {
-            "type": "box",
-            "layout": "vertical",
-            "contents": [
-                {
-                "type": "text",
-                "text": "Brown Cafe",
-                "size": "xl",
-                "weight": "bold"
-                },
-                {
-                "type": "box",
-                "layout": "baseline",
-                "margin": "md",
-                "contents": [
-                    {
-                    "type": "icon",
-                    "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
-                    "size": "sm"
-                    },
-                    {
-                    "type": "icon",
-                    "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
-                    "size": "sm"
-                    },
-                    {
-                    "type": "icon",
-                    "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
-                    "size": "sm"
-                    },
-                    {
-                    "type": "icon",
-                    "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
-                    "size": "sm"
-                    },
-                    {
-                    "type": "icon",
-                    "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png",
-                    "size": "sm"
-                    },
-                    {
-                    "type": "text",
-                    "text": "4.0",
-                    "flex": 0,
-                    "margin": "md",
-                    "size": "sm",
-                    "color": "#999999"
-                    }
-                ]
-                },
-                {
-                "type": "box",
-                "layout": "vertical",
-                "spacing": "sm",
-                "margin": "lg",
-                "contents": [
-                    {
-                    "type": "box",
-                    "layout": "baseline",
-                    "spacing": "sm",
-                    "contents": [
-                        {
-                        "type": "text",
-                        "text": "Place",
-                        "flex": 1,
-                        "size": "sm",
-                        "color": "#AAAAAA"
-                        },
-                        {
-                        "type": "text",
-                        "text": "Miraina Tower, 4-1-6 Shinjuku, Tokyo",
-                        "flex": 5,
-                        "size": "sm",
-                        "color": "#666666",
-                        "wrap": True
-                        }
-                    ]
-                    },
-                    {
-                    "type": "box",
-                    "layout": "baseline",
-                    "spacing": "sm",
-                    "contents": [
-                        {
-                        "type": "text",
-                        "text": "Time",
-                        "flex": 1,
-                        "size": "sm",
-                        "color": "#AAAAAA"
-                        },
-                        {
-                        "type": "text",
-                        "text": "10:00 - 23:00",
-                        "flex": 5,
-                        "size": "sm",
-                        "color": "#666666",
-                        "wrap": True
-                        }
-                    ]
-                    }
-                ]
-                }
-            ]
-            },
-            "footer": {
-            "type": "box",
-            "layout": "vertical",
-            "flex": 0,
-            "spacing": "sm",
-            "contents": [
-                {
-                "type": "button",
-                "action": {
-                    "type": "uri",
-                    "label": "CALL",
-                    "uri": "https://linecorp.com"
-                },
-                "height": "sm",
-                "style": "link"
-                },
-                {
-                "type": "button",
-                "action": {
-                    "type": "uri",
-                    "label": "WEBSITE",
-                    "uri": "https://linecorp.com"
-                },
-                "height": "sm",
-                "style": "link"
-                },
-                {
-                "type": "spacer",
-                "size": "sm"
-                }
-            ]
-            }
-        }
-        }
-        """
+        buttons_template_message = TemplateSendMessage(
+            alt_text='Buttons template',
+            template=ButtonsTemplate(
+                title='Menu',
+                text='Please select',
+                actions=[
+                    PostbackAction(
+                        label='postback',
+                        display_text='postback text',
+                        data='action=buy&itemid=1'
+                    )
+                ] 
+            )
+        )    
+        line_bot_api.push_message(event.reply_token, buttons_template_message)
 
-        que_bus = json.loads(payload)
+        # que_bus = json.loads(payload)
         print("ここ見てね")
-        print(que_bus)
-        line_bot_api.reply_message(
-        event.reply_token,FlexSendMessage(alt_text="hoge",contents=que_bus))
+        # print(que_bus)
+        # line_bot_api.reply_message(
+        # event.reply_token,FlexSendMessage(alt_text="hoge",contents=que_bus))
                 
         line_bot_api.reply_message(
             event.reply_token,
