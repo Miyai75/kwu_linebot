525923fb45a19052b3fa688a250605b48fbd45a2
Miyai75 <20241256@kyoto-wu.ac.jp>
2022-03-24 11:17:25 +0900

    sapo-to2

--- main.py
+++ main.py
@@ -57,62 +57,62 @@ def handle_message(event):
         event.reply_token,
         [TextSendMessage(text="天気だね"),TextSendMessage(text=weather)])
 
-    # if event.message.text == "大学生活に関する窓口":
-    #     line_bot_api.reply_message(
-    #     event.reply_token,
-    #     [TextSendMessage(text="進路 履修 インターンシップ 奨学金 各種証明書に関する対応窓口に関する情報を教えます！"),TextSendMessage(text=input("何について知りたいですか？"))])
-    #     word = input()
-    #     df = pd.read_csv("center2.csv")#csvファイルを読み込み
-    #     #print(list(df.loc[1]))
-
-    #     a = list#初期値
-
-    #     for index, data in df.iterrows():  # データフレームで行ごとにデータを取得
-    #         #print(index)
-    #         #print(data)
-    #         #print('--------')
-    #         if word == "進路":
-    #             a = list(df.loc[0])  # csvの1行目は項目なので2行目から数えるので2行目が[0]
-    #             break  # breakしないと6個同じものが出力される
-    #         elif word == "履修":
-    #             a = list(df.loc[1])
-    #             break
-    #         elif word == "インターンシップ":
-    #             a = list(df.loc[2])
-    #             break
-    #         elif word == "学費":
-    #             a = list(df.loc[3])
-    #             break
-    #         elif word == "奨学金":
-    #             a = list(df.loc[4])
-    #             break
-    #         elif word == "各種証明書":
-    #             a = list(df.loc[5])
-    #             break
-    #         else:
-    #             a = "いつでもどうぞ"
-    #             break
-        
-    #     line_bot_api.reply_message(
-    #     event.reply_token,
-    #     [TextSendMessage(text=a)])
-
-    if event.message.text == "バスの時刻":
-        print("フラグ")
-        print(bus_scene) 
+    if event.message.text == "大学生活に関する窓口":
         line_bot_api.reply_message(
         event.reply_token,
-        [TextSendMessage(text="登校しますか？下校しますか？"),TextSendMessage(text="1.市バス 2.プリンセスバス")])
-        bus_scene = 1
+        [TextSendMessage(text="進路 履修 インターンシップ 奨学金 各種証明書に関する対応窓口に関する情報を教えます！"),TextSendMessage(text=input("何について知りたいですか？"))])
+        word = input()
+        df = pd.read_csv("center2.csv")#csvファイルを読み込み
+        #print(list(df.loc[1]))
 
-    if bus_scene == 1:
-        bus_choices = {1:"市バス", 2:"プリンセスバス"} 
-        if event.message.text in bus_choices:
-            answer1 = bus_choices[event.message.text]
-            print(answer1)
-            line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=answer1))
+        a = list#初期値
+
+        for index, data in df.iterrows():  # データフレームで行ごとにデータを取得
+            #print(index)
+            #print(data)
+            #print('--------')
+            if event.message.text == "進路":
+                a = list(df.loc[0])  # csvの1行目は項目なので2行目から数えるので2行目が[0]
+                break  # breakしないと6個同じものが出力される
+            elif event.message.text == "履修":
+                a = list(df.loc[1])
+                break
+            elif event.message.text == "インターンシップ":
+                a = list(df.loc[2])
+                break
+            elif event.message.text == "学費":
+                a = list(df.loc[3])
+                break
+            elif event.message.text == "奨学金":
+                a = list(df.loc[4])
+                break
+            elif event.message.text == "各種証明書":
+                a = list(df.loc[5])
+                break
+            else:
+                a = "いつでもどうぞ"
+                break
+        
+        line_bot_api.reply_message(
+        event.reply_token,
+        [TextSendMessage(text=a)])
+
+    # if event.message.text == "バスの時刻":
+    #     print("フラグ")
+    #     print(bus_scene) 
+    #     line_bot_api.reply_message(
+    #     event.reply_token,
+    #     [TextSendMessage(text="登校しますか？下校しますか？"),TextSendMessage(text="1.市バス 2.プリンセスバス")])
+    #     bus_scene = 1
+
+    # if bus_scene == 1:
+    #     bus_choices = {1:"市バス", 2:"プリンセスバス"} 
+    #     if event.message.text in bus_choices:
+    #         answer1 = bus_choices[event.message.text]
+    #         print(answer1)
+    #         line_bot_api.reply_message(
+    #         event.reply_token,
+    #         TextSendMessage(text=answer1))
         
     line_bot_api.reply_message(
     event.reply_token,
